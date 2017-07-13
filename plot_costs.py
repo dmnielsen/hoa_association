f210678e5c3d1dd26c7276a8339741d1a2831fe3
dmnielsen <dmnielsen@gmail.com>
2017-07-12 21:47:01 -0500

    write plotting function to format figure

--- plot_costs.py
+++ plot_costs.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def import_data(filename):
     data = pd.read_csv(filename, sep=',', index_col=0)
+    data['Percent'] = data['Total']/data['Total'].sum()
     return data
 
 def plot_data_pandas(data):
@@ -30,15 +31,41 @@ def plot_data_mpl(data):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.barh(y_pos, data['Total'], align='center')
+    ax.set_frame_on(False)
+    ax.barh(y_pos, data['Total'], align='center', color='grey', lw=0)
 
     ax.set_yticks(y_pos)
     ax.set_yticklabels(data.index)
     ax.invert_yaxis()
 
+    rects = ax.patches
+    labels = ['{:.1f}%'.format(x*100) for x in data['Percent']]
+    for rect, label in zip(rects, labels):
+        print(rect.get_x(), rect.get_width())
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() + 1000,
+                rect.get_y() + height/2,
+                label, ha='left', va='center')
+
+
+    # format x ticks
+    x_ticks = [x*1e4 for x in np.arange(0,9,2)]
+    ax.set_xticks(x_ticks)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.xaxis.set_tick_params(width=2, length=7, color='grey', labelsize=12)
+
     formatter = FuncFormatter(format_xticks)
     ax.xaxis.set_major_formatter(formatter)
+    for tick in ax.get_xticks():
+        print(tick)
 
+    ax.set_xlabel('Total cost in dollars ($)', size=14)
+
+    # format yticks
+    ax.yaxis.set_ticks_position('none')
+    ax.yaxis.set_tick_params(labelsize=12)
+
+    plt.set_cmap('Greys')
     plt.savefig('2017_cost_plot_mpl.png', bbox_inches='tight')
 
 
