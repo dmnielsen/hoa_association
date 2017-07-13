cb68bbe447e60f0865d0b34c4703d172eae0eb37
dmnielsen <dmnielsen@gmail.com>
2017-07-12 21:47:29 -0500

    remove pandas plotting function

--- plot_costs.py
+++ plot_costs.py
@@ -9,19 +9,6 @@ def import_data(filename):
     data['Percent'] = data['Total']/data['Total'].sum()
     return data
 
-def plot_data_pandas(data):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    data.plot(kind='barh', legend=False)
-    print(['${:,}'.format(x, grouping=True) for x in data['Total']])
-    xticks = ax.get_xticks(); print(xticks)
-    ax.set_xticklabels(['${:,}'.format(x, grouping=True) for x in data['Total']])
-
-
-    ax.invert_yaxis()
-    plt.savefig('2017_cost_plot.png')
-
 def format_xticks(xtick, pos):
     # use custom ticker formatter to get $
     return '${:,.0f}'.format(xtick)
