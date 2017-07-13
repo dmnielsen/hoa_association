0d39c633222cc0579393282427fc0b9629b5830f
dmnielsen <dmnielsen@gmail.com>
2017-07-12 19:35:00 -0500

    add fuction to plot data using only matplotlib

--- plot_costs.py
+++ plot_costs.py
@@ -1,5 +1,6 @@
 import sys, os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import pandas as pd
 import numpy as np
 
@@ -20,8 +21,28 @@ def plot_data_pandas(data):
     ax.invert_yaxis()
     plt.savefig('2017_cost_plot.png')
 
+def format_xticks(xtick, pos):
+    # use custom ticker formatter to get $
+    return '${:,.0f}'.format(xtick)
+
+def plot_data_mpl(data):
+    y_pos = np.arange(len(data.index))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.barh(y_pos, data['Total'], align='center')
+
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(data.index)
+    ax.invert_yaxis()
+
+    formatter = FuncFormatter(format_xticks)
+    ax.xaxis.set_major_formatter(formatter)
+
+    plt.savefig('2017_cost_plot_mpl.png', bbox_inches='tight')
+
 
 if __name__ == '__main__':
     hoa_costs = import_data("2017_costs.csv")
     print(hoa_costs)
-    plot_data(hoa_costs)
+    plot_data_mpl(hoa_costs)
